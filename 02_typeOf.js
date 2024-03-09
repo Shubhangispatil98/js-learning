5240031ec5831347cd6f2114ff5887553eaeca80
Shubhangispatil98 <Shubhangispatil98@gmail.com>
2024-03-09 20:59:44 +0530

    typeOf changes Done
    typeOf changes Done

---
+++ 02_typeOf.js
@@ -0,0 +1,38 @@
+console.log("====Step1====");
+var bankName;
+typeOfBankName=typeof bankName;
+console.log("Type of bankName before initialization:", bankName);
+bankName = "SBI Bank";
+console.log("value after Initialization:", bankName);
+typeOfBankName = typeof bankName;
+console.log("Type of bankName is:", typeOfBankName);
+
+console.log("====Step2====");
+var marks = "90%";
+console.log("value of marks:", marks);
+typeOfMarks = typeof marks;
+console.log("Type of marks is:", typeOfMarks);
+
+console.log("====Step3====");
+var isWorking;
+console.log("value of isWorking before Initialization:", isWorking);
+typeOfIsWorking = typeof isWorking;
+console.log("Type of isWorking before Initialization:", typeOfIsWorking);
+isWorking = true;
+console.log("value of isWorking after Initialization:", isWorking);
+typeOfIsWorking = typeof isWorking;
+console.log("Type of isWorking after Initialization:", typeOfIsWorking);
+
+console.log("====Step4====");
+var totalCount = "Hundred and Seven";
+console.log("value of totalCount:", totalCount);
+typeOfTotalCount = typeof totalCount;
+console.log("Type of totalCount:", typeOfTotalCount);
+totalCount = 107;
+console.log("value of totalCount after Update:", totalCount);
+typeOfTotalCount = typeof totalCount;
+console.log("TypeOf totalCount after Update:", typeOfTotalCount);
+
+
+
+
