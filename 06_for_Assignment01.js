b18df188cfd1b148ccdd5a35ab89b770577be4cd
Shubhangispatil98 <Shubhangispatil98@gmail.com>
2024-03-19 23:18:46 +0530

    for loop Assignment done
    for loop Assignment done

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,36 @@
+// WAP to print numbers from 5 to 15 by incrementing 1
+console.log("---numbers from 5 to 15---");
+for (let index = 5; index <= 15; index++)
+{
+    console.log(index);
+}
+
+//WAP to print numbers from 50 to 40 decrementing by 1
+console.log("---numbers from 50 to 40---");
+for (let index = 50; index >= 40; index--)
+{
+    console.log(index);
+}
+console.log("---first 15 odd numbers---");
+for (let index = 1; index <= 30; index = index + 2)
+{
+    console.log(index);
+}
+console.log("---first 10 even numbers---");
+for (let index = 0; index <= 20; index = index + 2) {
+  console.log(index);
+}
+console.log("---table of 5---");
+for (let index = 5; index <= 50; index = index + 5)
+{
+    console.log(index);
+}
+console.log("---table of 10---");
+for (let index = 10; index <=100; index=index+10) {
+    console.log(index);
+    
+}
+console.log("---table of 10 in reverse---");
+for (let index = 100; index >=10; index = index -10) {
+  console.log(index);
+}
